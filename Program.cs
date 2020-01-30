c922897c4f7482bee3fd68a573f6f89a9a0926c7
Ayrton Ciancimino <ajciancimino@usf.edu>
2020-01-30 18:05:33 -0500

    Final Commit

--- Program.cs
+++ Program.cs
@@ -18,33 +18,39 @@ namespace Conditional_Statements
                 // This variable stores a refined value after it has been parsed
                 int grade = int.Parse(input);
 
-                //A series of conditionals to evaluate the grade and display appropriate letter grade
-                if (98 <= grade)
-                    Console.WriteLine("A+");
-                else if (92 <= grade)
-                    Console.WriteLine("A");
-                else if (90 <= grade)
-                    Console.WriteLine("A-");
-                else if (88 <= grade)
-                    Console.WriteLine("B+");
-                else if (82 <= grade)
-                    Console.WriteLine("B");
-                else if (80 <= grade)
-                    Console.WriteLine("B-");
-                else if (78 <= grade)
-                    Console.WriteLine("C+");
-                else if (72 <= grade)
-                    Console.WriteLine("C");
-                else if (70 <= grade)
-                    Console.WriteLine("C-");
-                else if (68 <= grade)
-                    Console.WriteLine("D+");
-                else if (62 <= grade)
-                    Console.WriteLine("D");
-                else if (60 <= grade)
-                    Console.WriteLine("D-");
+                if ((grade >= 0) && (grade <= 100))
+                {
+                    //A series of conditionals to evaluate the grade and display appropriate letter grade
+                    if (98 <= grade)
+                        Console.WriteLine("A+");
+                    else if (92 <= grade)
+                        Console.WriteLine("A");
+                    else if (90 <= grade)
+                        Console.WriteLine("A-");
+                    else if (88 <= grade)
+                        Console.WriteLine("B+");
+                    else if (82 <= grade)
+                        Console.WriteLine("B");
+                    else if (80 <= grade)
+                        Console.WriteLine("B-");
+                    else if (78 <= grade)
+                        Console.WriteLine("C+");
+                    else if (72 <= grade)
+                        Console.WriteLine("C");
+                    else if (70 <= grade)
+                        Console.WriteLine("C-");
+                    else if (68 <= grade)
+                        Console.WriteLine("D+");
+                    else if (62 <= grade)
+                        Console.WriteLine("D");
+                    else if (60 <= grade)
+                        Console.WriteLine("D-");
+                    else if (60 > grade)
+                        Console.WriteLine("F");
+                }
                 else
-                    Console.WriteLine("F");
+                    Console.WriteLine("Grade must be between 0 and 100");
+
             }
             catch
             {
